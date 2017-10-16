ecf4d963f4fe9447a4c66a661c41200874db3a97
joako <jquezadarios@gmail.com>
2017-10-16 15:09:25 -0300

    mejora de diseño.
    se mejoro el diseño del login y de nutricion.html

--- login/index.php
+++ login/index.php
@@ -39,48 +39,54 @@
   <div class="container">
     <div class="row">
       <div class="col m12">
-        <div class="card-panel #ec407a pink lighten-1">Ingrese</div>
+        <div class="card-panel #ec407a pink lighten-1"><p style="color:#ffffff; font-size:30px ">Inicio de sesion.</p></div>
       </div>
- 
-<center><div class="tit"><h2 style="color: #0000FF; ">Inicio de sesión</h2>
+ 	</div>
+
+
+
+<div class="row">
+      <div width:800px>
+        <div class="card-panel #ec407a pink lighten-1">	
 		<center><div class="Ingreso">
 
-	<table border="0" align="left" valign="middle">
+	<table>
 		<tr>
 		<td rowspan=2>
 		<form action="validar.php" method="post">
 
 		<table border="0" align="center">
 
-		<tr><td><label style="font-size: 14pt" align="center"><b>Correo: </b></label></td>
-			<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
-		<tr><td><label style="font-size: 14pt" align="center"><b>Contraseña: </b></label></td>
-			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
+		<tr><td><label style="font-size: 14pt;color:#ffffff; width:30px;" align="center"><b>Correo: </b></label></td>  
+			<td width=80> <input class="form-group has-success" style="border-radius:15px;color:#ffffff;" type="text" name="mail" placeholder="Ingresa tu correo"  ></td></tr> </div>
+		<tr><td><label style="font-size: 14pt;color:#ffffff;" align="center"><b>Contraseña: </b></label></td>
+			<td witdh=80><input style="border-radius:30px; width:300px;color:#ffffff;"  type="password" name="pass" placeholder="Ingresa contraseña"></td></tr>
 		<tr><td></td>
 			<td width=80 ><input class="btn btn-primary" type="submit" value="Aceptar"></td>
 			</tr></tr></table>
 		</form>
 <br>
+
 <!-- formulario registro -->
 
 <form method="post" action="" >
   <fieldset>
-    <legend  style="font-size: 18pt"><b>Registro</b></legend>
+    <legend  style="font-size: 18pt; color:#ffffff;"><b>Registro</b></legend>
     <div class="form-group">
-      <label style="font-size: 14pt"><b>Ingresa tu nombre</b></label>
-      <input type="text" name="realname" class="form-control" placeholder="Ingresa tu nombre" />
+      <label style="font-size: 14pt; color:#ffffff;"><b>Ingresa tu nombre</b></label>
+      <input style="color:#ffffff;" type="text" name="realname" class="form-control" placeholder="Ingresa tu nombre" />
     </div>
     <div class="form-group">
-      <label style="font-size: 14pt"><b>Ingresa tu email</b></label>
-      <input type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
+      <label style="font-size: 14pt; color:#ffffff;"><b>Ingresa tu email</b></label>
+      <input style="color:#ffffff;"type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
     </div>
     <div class="form-group">
-      <label style="font-size: 14pt"><b>Ingresa tu Password</b></label>
-      <input type="password" name="pass" class="form-control"  placeholder="Ingresa contraseña" />
+      <label style="font-size: 14pt; color:#ffffff;"><b>Ingresa tu Password</b></label>
+      <input style="color:#ffffff;" type="password" name="pass" class="form-control"  placeholder="Ingresa contraseña" />
     </div>
     <div class="form-group">
-      <label style="font-size: 14pt"><b>Repite tu contraseña</b></label>
-      <input type="password" name="rpass" class="form-control" required placeholder="repite contraseña" />
+      <label style="font-size: 14pt; color:#ffffff;"><b>Repite tu contraseña</b></label>
+      <input  style="color:#ffffff;" type="password" name="rpass" class="form-control" required placeholder="repite contraseña" />
     </div>
       
     </div>
@@ -90,6 +96,13 @@
   </fieldset>
 </form>
 </div>
+</table>
+
+ </div>
+      </div>
+    </div>
+ </div>
+
 <?php
 		if(isset($_POST['submit'])){
 			require("registro.php");
