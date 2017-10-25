d512e755ef51852580f602461ef3159bf84c423c
joako <jquezadarios@gmail.com>
2017-10-25 16:17:35 -0300

    agregado 2 graficos, actualizacion bd
    agregue 2 graficos pero por ahora solo 1 conecte a la bd

--- PÁG. NUTRICION/nutricion.php
+++ PÁG. NUTRICION/nutricion.php
@@ -255,6 +255,198 @@ if ($pasaste<0) {
 });
 </script>
 
+
+<?php
+$con=mysqli_connect("localhost", "root","", "academ");
+$email=$_SESSION['email'];
+$sql="SELECT * FROM seguimiento_usuario,login WHERE login.email='$email' and seguimiento_usuario.id=login.id and fecha=curdate()";
+if ($result=mysqli_query($con,$sql))
+  {
+$totaldiaP=0;
+$totaldiaC=0;
+$totaldiaG=0;
+$totaldiaS=0;
+$totaldiaCA=0;
+$totaldiaA=0;
+$totaldiaV=0;
+  while ($row=mysqli_fetch_array($result))
+    {
+    $totaldiaP=$totaldiaP+$row["Proteinas"];
+    $totaldiaC=$totaldiaC+$row["Colesterol"];
+    $totaldiaG=$totaldiaG+$row["Grasas_totales"];
+    $totaldiaS=$totaldiaS+$row["Sodio"];
+    $totaldiaCA=$totaldiaCA+$row["Carbohidratos"];
+    $totaldiaA=$totaldiaA+$row["Azucar"];
+    $totaldiaV=$totaldiaV+$row["Vitaminas"];
+	} }
+?>
+
+
+<?php
+$con=mysqli_connect("localhost", "root","", "academ");
+$email=$_SESSION['email'];
+foreach (range(0, 12, 1) as $número) {
+$sql="SELECT * FROM seguimiento_usuario,login WHERE login.email='$email' and seguimiento_usuario.id=login.id and MONTH(fecha)=0$número";
+if ($result=mysqli_query($con,$sql))
+  {
+$totalP[$número]=0;
+$totalC[$número]=0;
+$totalG[$número]=0;
+$totalS[$número]=0;
+$totalCA[$número]=0;
+$totalA[$número]=0;
+$totalV[$número]=0;
+  while ($row=mysqli_fetch_array($result))
+    {
+    $totalP[$número]=$totalP[$número]+$row["Proteinas"];
+    $totalC[$número]=$totalC[$número]+$row["Colesterol"];
+    $totalG[$número]=$totalG[$número]+$row["Grasas_totales"];
+    $totalS[$número]=$totalS[$número]+$row["Sodio"];
+    $totalCA[$número]=$totalCA[$número]+$row["Carbohidratos"];
+    $totalA[$número]=$totalA[$número]+$row["Azucar"];
+    $totalV[$número]=$totalV[$número]+$row["Vitaminas"];
+	} } }
+?>
+
+
+
+<div class="row" style="display: inline-block; 	width:560px;">
+         <div class="col s12 m6" style="width:100%;">
+         <div class="card #ec407a pink lighten-1">
+            <div style="height:380px"> <div class="card-content white-text">
+              <span class="card-title ">Nutrientes consumidos dia:</span>
+<div class="card #ec407a white lighten-1">
+	<canvas id="bar-chart" width="800" height="450"></canvas>
+</div>
+<script>
+
+var totaldiaP = "<?php echo $totaldiaP; ?>";
+var totaldiaC = "<?php echo $totaldiaC; ?>";
+var totaldiaG = "<?php echo $totaldiaG; ?>";
+var totaldiaS = "<?php echo $totaldiaS; ?>";
+var totaldiaCA = "<?php echo $totaldiaCA; ?>";
+var totaldiaA = "<?php echo $totaldiaA; ?>";
+var totaldiaV = "<?php echo $totaldiaV; ?>";
+
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+	
+    data: {
+      labels: ["Proteinas(g)", "Colesterol(mg)", "Grasas_totales(g)", "Sodio(mg)", "Carbohidratos(g)","Azucar(g)","Vitaminas(mg)"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [totaldiaP,totaldiaC,totaldiaG,totaldiaS,totaldiaCA,totaldiaA,totaldiaV]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: ''
+      }
+    }
+});
+
+</script>
+
+
+
+
+
+
+
+		<div > </canvas></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div class="row" style="display: inline-block; 	width:600px;">
+         <div class="col s12 m6" style="width:100%;">
+         <div class="card #ec407a pink lighten-1">
+            <div style="height:380px"> <div class="card-content white-text">
+              <span class="card-title ">Nutrientes consumidos mes:</span>
+<div class="card #ec407a white lighten-1">
+<canvas id="line-chart" width="800" height="420"></canvas>
+</div>
+<script>
+
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    datasets: [{ 
+        data: [86,114,106,106,107,111,133,221,783,2478],
+        label: "Proteinas(g)",
+        borderColor: "#3e95cd",
+        fill: false
+      }, { 
+        data: [282,350,411,502,635,809,947,1402,3700,5267],
+        label: "Colesterol(mg)",
+        borderColor: "#8e5ea2",
+        fill: false
+      }, { 
+        data: [168,170,178,190,203,276,408,547,675,734],
+        label: "Grasas_totales(g)",
+        borderColor: "#3cba9f",
+        fill: false
+      }, { 
+        data: [40,20,10,16,24,38,74,167,508,784],
+        label: "Sodio(mg)",
+        borderColor: "#e8c3b9",
+        fill: false
+      }, { 
+        data: [6,3,2,2,7,26,82,172,312,433],
+        label: "Carbohidratos(g)",
+        borderColor: "#c45850",
+        fill: false
+      },  { 
+        data: [6,3,2,2,7,26,82,172,312,433],
+        label: "Azucar(g)",
+        borderColor: "#c45850",
+        fill: false},
+ { 
+        data: [6,3,2,2,7,26,82,172,312,433],
+        label: "Vitaminas(mg)",
+        borderColor: "#c45850",
+        fill: false}
+    ]
+  },
+  options: {
+    title: {
+      display: false,
+      text: ''
+    }
+  }
+});
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+		<div > </canvas></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
 <!-- insertar alimento -->
       </div>
 	</div>
@@ -269,10 +461,10 @@ if ($pasaste<0) {
 <div class="form-group">
     <div class="input-group">
 	<h3> buscar: </h3>
-     <input  type="text" id="search_text" placeholder="Busca alimentos..." class="form-control" />
-     <h3 >cantidad (g): </h3><input type="text" id="cant" name="cant" placeholder="cantidad:" class="form-control" />
+     <input style="border-radius:30px; width:300px;color:#ffffff;font-size:15px;" type="text" id="search_text" placeholder="Busca alimentos..." class="form-control" />
+     <h3 >cantidad (g): </h3><input style="border-radius:30px; width:300px;color:#ffffff;font-size:15px;" type="text" id="cant" name="cant" placeholder="cantidad:" class="form-control" />
     </div>
-<button   class="btn btn-danger" name="submit" type="submit"> elegir </button>
+<button  class="btn btn-danger" name="submit" type="submit"> elegir </button>
    </div>
    <div id="result"></div>
               </div>
@@ -291,7 +483,7 @@ if ($pasaste<0) {
 
 $con=mysqli_connect("localhost", "root","", "academ");
 $email=$_SESSION['email'];
-$sql="INSERT INTO seguimiento_usuario VALUES('',((SELECT Calorias FROM alimentos WHERE ID_A='$id')/100)*$cant,(SELECT id FROM login WHERE email='$email'),CURRENT_TIMESTAMP())";
+$sql="INSERT INTO seguimiento_usuario VALUES('',((SELECT Calorias FROM alimentos WHERE ID_A='$id')/100)*$cant,(SELECT id FROM login WHERE email='$email'),CURRENT_TIMESTAMP(),((SELECT Proteinas FROM alimentos WHERE ID_A='$id')/100)*$cant,((SELECT Colesterol FROM alimentos WHERE ID_A='$id')/100)*$cant,((SELECT Grasas_totales FROM alimentos WHERE ID_A='$id')/100)*$cant,((SELECT Sodio FROM alimentos WHERE ID_A='$id')/100)*$cant,((SELECT Carbohidratos FROM alimentos WHERE ID_A='$id')/100)*$cant,((SELECT Azucar FROM alimentos WHERE ID_A='$id')/100)*$cant,((SELECT Vitaminas FROM alimentos WHERE ID_A='$id')/100)*$cant)";
 mysqli_query($con,$sql);
 echo "<script>location.href='nutricion.php';</script>";
 
